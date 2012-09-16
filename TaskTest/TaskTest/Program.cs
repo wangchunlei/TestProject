12105480b068f11c0206e5d4f92d44ab40f2c450
wangchunlei <wcl_orange@163.com>
2012-09-16 19:27:47 +0800

    test

---
+++ TaskTest/TaskTest/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var thread = new Thread(Go);
+            thread.Start();
+            thread.Join();
+            var blocked = (thread.ThreadState & ThreadState.WaitSleepJoin) != 0;
+            Console.WriteLine("thread run over");
+
+
+            bool done = false;
+            ThreadStart action = () =>
+            {
+                if (!done)
+                {
+                    done = true;
+                    Console.WriteLine("Done");
+                }
+            };
+            new Thread(action).Start();
+            action();
+            Console.ReadKey();
+        }
+
+        static void Go()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Yield();
+                Console.Write("y");
+            }
+        }
+    }
+}
