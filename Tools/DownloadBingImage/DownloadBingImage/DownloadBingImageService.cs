fae4b664487a2ee0b0791db377848c5a96489f2d
wangchunlei <wcl_orange@msn.com>
2012-11-02 17:00:55 +0800

    add networkavailable event

--- Tools/DownloadBingImage/DownloadBingImage/DownloadBingImageService.cs
+++ Tools/DownloadBingImage/DownloadBingImage/DownloadBingImageService.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading;
@@ -31,13 +32,26 @@ namespace DownloadBingImage
             int intervalMin = int.MaxValue;
             int.TryParse(interval, out intervalMin);
             log.Debug("服务启动，下次下载" + intervalMin + "分钟后");
-            timer = new Timer(delegate
-            {
-                log.Debug(string.Format("开始下载"));
-                BingImages.DownLoadImages();
-            }, null, TimeSpan.FromMilliseconds(10), TimeSpan.FromMinutes(intervalMin));
+            NetworkChange.NetworkAvailabilityChanged += (o, eventArgs) => log.Debug(string.Format("网络发生变化,当前网络{0}可用", eventArgs.IsAvailable ? "" : "不"));
+            NetworkChange.NetworkAddressChanged += (o, eventArgs) => log.Debug("IP地址发生变化");
+            timer = new Timer(state =>
+                {
+                    if (IsNetworkAvailable())
+                    {
+                        log.Debug(string.Format("开始下载"));
+                        BingImages.DownLoadImages();
+                    }
+                    else
+                    {
+                        log.Debug("当前网络不可用，等待网络连接");
+                    }
+                }, null, TimeSpan.FromMilliseconds(10), TimeSpan.FromMinutes(intervalMin));
         }
 
+        private bool IsNetworkAvailable()
+        {
+            return System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable();
+        }
         protected override void OnStop()
         {
             timer.Dispose();
