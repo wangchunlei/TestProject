83bdc875ebd7295fa55f43044bd4db0654112d25
wangchunlei <wcl_orange@163.com>
2013-05-10 17:34:47 +0800

    cmmit

---
+++ Labs/LogServiceWebApi/LogViewer/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace LogViewer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            Log4netSqlite.Program.PersistentData();
+            var data = Log4netSqlite.Program.Query("").ToList();
+            return View(data);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Chart()
+        {
+            var basicChart = new Chart(width: 600, height: 400)
+                .AddTitle("Chart Title")
+                .AddSeries(
+                    name: "Employee",
+                    xValue: new[] { "Peter", "Andrew", "Julie", "Mary", "Dave" },
+                    yValues: new[] { "2", "6", "4", "5", "3" }).GetBytes("png");
+            return File(basicChart, "image/png");
+        }
+    }
+}
