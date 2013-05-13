a70fb4a9a19ec24f8a8c69994907fbd36bee4ec0
wangchunlei <wcl_orange@163.com>
2013-05-13 19:18:54 +0800

    commit

--- Labs/LogServiceWebApi/LogViewer/Controllers/HomeController.cs
+++ Labs/LogServiceWebApi/LogViewer/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Dynamic;
 using System.Linq;
 using System.Web;
 using System.Web.Helpers;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace LogViewer.Controllers
 {
@@ -16,13 +19,28 @@ namespace LogViewer.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult About(int page = 1)
         {
+            const int pageSize = 2;
             Log4netSqlite.Program.PersistentData();
             var data = Log4netSqlite.Program.Query("").ToList();
-            return View(data);
+            var viewData = new
+                {
+                    PageSize = pageSize,
+                    PageNumber = page,
+                    Products = data.Skip((page - 1) * pageSize).Take(pageSize),
+                    TotalRows = data.Count
+                };
+            return View(ToExpando(viewData));
         }
-
+        public static ExpandoObject ToExpando(object anonymousObject)
+        {
+            IDictionary<string, object> anonymousDictionary = new RouteValueDictionary(anonymousObject);
+            IDictionary<string, object> expando = new ExpandoObject();
+            foreach (var item in anonymousDictionary)
+                expando.Add(item);
+            return (ExpandoObject)expando;
+        }
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
@@ -32,13 +50,28 @@ namespace LogViewer.Controllers
 
         public ActionResult Chart()
         {
-            var basicChart = new Chart(width: 600, height: 400)
+            var basicChart = new Chart(width: 440, height: 400)
                 .AddTitle("Chart Title")
                 .AddSeries(
                     name: "Employee",
                     xValue: new[] { "Peter", "Andrew", "Julie", "Mary", "Dave" },
                     yValues: new[] { "2", "6", "4", "5", "3" }).GetBytes("png");
             return File(basicChart, "image/png");
+        }
+
+        public ActionResult MyChart()
+        {
+            var dataSet = new DataSet();
+            dataSet.ReadXmlSchema(Server.MapPath("~/App_Data/data.xsd"));
+            dataSet.ReadXml(Server.MapPath("~/App_Data/data.xml"));
+            var dataView = new DataView(dataSet.Tables[0]);
+
+            var myChart = new Chart(width: 440, height: 400)
+                .AddTitle("Sales Per Employee")
+                .AddSeries("Default", chartType: "Pie",
+                    xValue: dataView, xField: "Name",
+                    yValues: dataView, yFields: "Sales").GetBytes("png");
+            return File(myChart, "image/png");
         }
     }
 }
