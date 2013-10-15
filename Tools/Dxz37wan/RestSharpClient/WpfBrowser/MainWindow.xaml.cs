4db239372791bcefd99cd67f2b8444b1ab0f7b70
wangchunlei <wcl_orange@163.com>
2013-10-15 17:52:23 +0800

    add slicent

--- Tools/Dxz37wan/RestSharpClient/WpfBrowser/MainWindow.xaml.cs
+++ Tools/Dxz37wan/RestSharpClient/WpfBrowser/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -31,6 +33,12 @@ namespace WpfBrowser
                     Button_Click(s, e);
                 }
             };
+            this.webBrowser.Navigated += webBrowser_Navigated;
+        }
+
+        void webBrowser_Navigated(object sender, NavigationEventArgs e)
+        {
+            Help.SetSilent(this.webBrowser, true); // make it silent
         }
 
         private void Login()
@@ -97,6 +105,38 @@ namespace WpfBrowser
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
             this.webBrowser.Visibility = Visibility.Visible;
+        }
+    }
+
+    public class Help
+    {
+        public static void SetSilent(WebBrowser browser, bool silent)
+        {
+            if (browser == null)
+                throw new ArgumentNullException("browser");
+
+            // get an IWebBrowser2 from the document
+            IOleServiceProvider sp = browser.Document as IOleServiceProvider;
+            if (sp != null)
+            {
+                Guid IID_IWebBrowserApp = new Guid("0002DF05-0000-0000-C000-000000000046");
+                Guid IID_IWebBrowser2 = new Guid("D30C1661-CDAF-11d0-8A3E-00C04FC9E26E");
+
+                object webBrowser;
+                sp.QueryService(ref IID_IWebBrowserApp, ref IID_IWebBrowser2, out webBrowser);
+                if (webBrowser != null)
+                {
+                    webBrowser.GetType().InvokeMember("Silent", BindingFlags.Instance | BindingFlags.Public | BindingFlags.PutDispProperty, null, webBrowser, new object[] { silent });
+                }
+            }
+        }
+
+
+        [ComImport, Guid("6D5140C1-7436-11CE-8034-00AA006009FA"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        private interface IOleServiceProvider
+        {
+            [PreserveSig]
+            int QueryService([In] ref Guid guidService, [In] ref Guid riid, [MarshalAs(UnmanagedType.IDispatch)] out object ppvObject);
         }
     }
 }
