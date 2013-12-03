89e8dfca9922e1e365f86929717a9e72f3bfe2b0
wangchunlei <wcl_orange@163.com>
2013-12-03 17:16:13 +0800

    add new project

---
+++ Labs/WebApi.SelfHost.Server/WebApi.SelfHost.Server/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Dispatcher;
+using System.Web.Http.SelfHost;
+
+namespace WebApi.SelfHost.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new HttpSelfHostConfiguration("http://localhost:8080");
+
+            var assemblyResolver = new AssembliesResolver();
+            config.Services.Replace(typeof(IAssembliesResolver), assemblyResolver);
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}"
+                );
+
+            using (HttpSelfHostServer server = new HttpSelfHostServer(config))
+            {
+                server.OpenAsync().Wait();
+                Console.WriteLine("Press Enter to quit.");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class AssembliesResolver : DefaultAssembliesResolver
+    {
+        public override ICollection<Assembly> GetAssemblies()
+        {
+            ICollection<Assembly> baseAssemblies = base.GetAssemblies();
+            List<Assembly> assemblies = new List<Assembly>(baseAssemblies);
+            var controllersAssembly = Assembly.LoadFrom(@"C:\libs\controllers\ControllersLibrary.dll");
+            baseAssemblies.Add(controllersAssembly);
+            return assemblies;
+        }
+    }
+
+    public class CustomAssemblyResolver : IAssembliesResolver
+    {
+        public ICollection<Assembly> GetAssemblies()
+        {
+            List<Assembly> baseAssemblies = AppDomain.CurrentDomain.GetAssemblies().ToList();
+            var controllersAssembly = Assembly.LoadFrom(@"C:\libs\controllers\ControllersLibrary.dll");
+            baseAssemblies.Add(controllersAssembly);
+            return baseAssemblies;
+        }
+    }
+}
