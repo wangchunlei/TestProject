244e1a96ec1ad84642df081e590f84c85a54a4ee
wangchunlei <wcl_orange@163.com>
2014-03-13 13:24:07 +0800

    add get windows credentials

--- Test/AutoLoginTest/AutoLoginTest/Program.cs
+++ Test/AutoLoginTest/AutoLoginTest/Program.cs
@@ -1,6 +1,7 @@
 ﻿using CredentialManagement;
 using System;
 using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -12,10 +13,31 @@ namespace AutoLoginTest
     {
         static void Main(string[] args)
         {
+            //using (PrincipalContext context = new PrincipalContext(ContextType.Machine, "192.168.20.30",null,ContextOptions.Negotiate))
+            //{
+            //    //if (!currentUser.ToLower().StartsWith("lanxum\\"))
+            //    //{
+            //    //    Validate(domain, context, new Exception("当前用户未登录域"));
+            //    //}
+            //    //else
+            //    {
+            //        try
+            //        {
+            //            var upUser = GroupPrincipal.FindByIdentity(context, IdentityType.SamAccountName, "wcl");
+            //        }
+            //        catch (Exception ex)
+            //        {
+            //            //Validate(domain, context, ex);
+            //        }
+            //    }
+            //}
+
             var cm = new VistaPrompt();
             //cm.Domain = "lanxum";
             cm.SaveChecked = true;
             cm.ShowSaveCheckBox = true;
+            cm.Title = @"指定已授权的 域(计算机)\用户";
+            cm.Message = "123213";
             DialogResult rs = cm.ShowDialog();
             
             //CredentialManagement();
@@ -35,6 +57,16 @@ namespace AutoLoginTest
                     Console.WriteLine(t.Result);
                 }
             });
+
+            var c = new Credential()
+            {
+                Target = "192.168.20.30",
+                Type = CredentialType.DomainPassword,//windows 凭证 Generic 普通凭证
+                PersistanceType = PersistanceType.Enterprise,//永久
+                Username = "zhangzhongsheng@lanxum.com",
+                SecurePassword = cm.SecurePassword,
+            };
+            c.Save();
 
             Console.ReadKey();
         }
@@ -65,6 +97,47 @@ namespace AutoLoginTest
                 Password = "zhangzs"
             };
             c.Save();
+        }
+
+        public static System.Net.NetworkCredential GetCredential(string serverip)
+        {
+            System.Net.NetworkCredential credentials = null;
+            BaseCredentialsPrompt prompt = null;
+            if (IsWinVistaOrHigher())
+            {
+                prompt = new VistaPrompt();
+            }
+            else
+            {
+                prompt = new XPPrompt();
+            }
+            prompt.SaveChecked = true;
+            prompt.ShowSaveCheckBox = true;
+            prompt.Title = @"指定已授权的 域(计算机)\用户";
+
+            if (prompt.ShowDialog() == DialogResult.OK)
+            {
+                credentials = new System.Net.NetworkCredential(prompt.Username, prompt.SecurePassword);
+                if (prompt.SaveChecked)
+                {
+                    var cm = new Credential()
+                    {
+                        Target = serverip,
+                        Type = CredentialType.DomainPassword,//windows 凭证 Generic 普通凭证
+                        PersistanceType = PersistanceType.Enterprise,//永久
+                        Username = prompt.Username,
+                        SecurePassword = prompt.SecurePassword
+                    };
+                    cm.Save();
+                }
+                return credentials;
+            }
+            return null;
+        }
+        private static bool IsWinVistaOrHigher()
+        {
+            OperatingSystem OS = Environment.OSVersion;
+            return (OS.Platform == PlatformID.Win32NT) && (OS.Version.Major >= 6);
         }
     }
 }
