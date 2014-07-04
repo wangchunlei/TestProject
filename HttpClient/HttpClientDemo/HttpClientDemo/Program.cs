f93c2a3d50ba6832e4508b7e4365822fb9230bf2
wangchunlei <wcl_orange@163.com>
2014-07-04 15:38:56 +0800

    commit

---
+++ HttpClient/HttpClientDemo/HttpClientDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClientDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClientHandler clientHandler = new HttpClientHandler();
+
+            using (var client = new HttpClient(clientHandler))
+            {
+                client.BaseAddress = new Uri("http://www.baidu.com");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = client.GetStringAsync("").ContinueWith((content) =>
+                {
+                    Console.WriteLine(2);
+                });
+                Console.WriteLine(1);
+                Console.ReadKey(false);
+            }
+        }
+    }
+}
