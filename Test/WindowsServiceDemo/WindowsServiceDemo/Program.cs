cf47ef9f153fcd7b4ac679201a1c8378755b0eef
wangchunlei <wcl_orange@163.com>
2014-08-29 16:32:46 +0800

    commit:InterceptKeys

---
+++ Test/WindowsServiceDemo/WindowsServiceDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsServiceDemo
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            var ser = new Service1();
+            if (Environment.UserInteractive)
+            {
+                ser.Start(null);
+            }
+            else
+            {
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[]
+                {
+                    ser
+                };
+                ServiceBase.Run(ServicesToRun);
+            }
+            Console.ReadKey(false);
+        }
+    }
+}
---
+++ Test/WindowsServiceDemo/WindowsServiceDemo/Service1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using Domas.DAP.ADF.AutoUpdate;
+
+namespace WindowsServiceDemo
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        public void Start(string[] args)
+        {
+            this.OnStart(args);
+        }
+        protected override void OnStart(string[] args)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                var uiExeFileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "InterceptKeys.exe");
+                Interop.CreateProcess(uiExeFileName);
+            });
+        }
+
+        protected override void OnStop()
+        {
+            var uiExeFileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "InterceptKeys.exe");
+           var process= Process.GetProcessesByName("InterceptKeys").FirstOrDefault();
+            if (process!=null)
+            {
+                process.Kill();
+                process.WaitForExit((int)TimeSpan.FromSeconds(10).TotalMilliseconds);
+            }
+        }
+    }
+}
